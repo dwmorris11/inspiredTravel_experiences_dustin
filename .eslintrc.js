5790341a6e24c704cdcafce47d44836495b6bf2a
Dustin Morris <dustinnuc@gmail.com>
2020-06-18 01:44:48 -0500

    Updated eslint to ignore the bundle.js Created a new destination model to map experiences to destinations Updated package.json by moving morgan to dependencies based on linter Started developing the routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,4 +24,5 @@ module.exports = {
   ],
   rules: {
   },
+  ignorePatterns: [ 'bundle.js' ],
 };
--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
-const path = require('path');
-const { client } = require('../s3client.js');
-const { db } = require('../database/index.js');
-const axios = require('axios');
-const morgan = require('morgan');
 const express = require('express');
+const { db } = require('../database/index.js');
+// const axios = require('axios');
+const morgan = require('morgan');
+// const { client } = require('../s3client.js');
 
 const app = express();
 const port = 3636;
 
 app.use(morgan('dev'));
 
-app.
+// app.get('/:id', (req, res) => {
+//   const id = req.params.id;
 
-app.listen(port, () = {
+// })
+
+app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-})
+});
