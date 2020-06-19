b786183a77d587d22dd763d1beb431a813d1046c
Dustin Morris <dustinnuc@gmail.com>
2020-06-18 21:49:45 -0500

    Created experience react module skeleton

---
+++ client/experience_module.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const Experience = () => (
+<div className='Experience_Container'>
+<div className='Experience'>
+  <div className='Heart_Container'>
+    <div className='Heart'>
+      <span>
+        <span>
+          <span>
+          </span>
+        </span>
+        <span>
+          <span>
+          </span>
+        </span>
+      </span>
+   </div>
+  </div>
+ <div className='Photo'><a><img /></a>
+ </div>
+ <div className='Description_Container'>
+  <div className='Description'>
+    <a><span id='Description_Text'></span></a>
+    <span className='Review'></span>
+  </div>
+ </div>
+ <a><div className='Review_Container'>
+     <span className='Review_Circles'></span>
+     <span className='Review_Count'></span>
+    </div></a>
+    <div className='Footer_Container'>
+      <div className='Cost_Container'>
+        <span className='Cost'></span>
+      </div>
+      <div className='More_Info'>
+        <button className='More_Info_Button'></button>
+      </div>
+    </div>
+  <div className='QuickView'>
+    <div className='QuickView_Button_Container'>
+      <div>
+        <div>
+          <button className='QuickView_Button'>
+            <span className='QuickView_Eye'></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+)
